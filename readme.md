04ae0c2a292f48dd60bf36899fba6c5d6fc32330
Robert Lord <robert@lord.io>
2017-10-17 22:33:36 -0500

    Fix bug with not printing live cargo output

---
+++ readme.md
@@ -0,0 +1,5 @@
+<p align="center">
+  <img src="https://raw.githubusercontent.com/lord/img/master/logo-cargowasm.png" alt="wargo: Easy Rust to Webassembly" width="226">
+  <br>
+  <a href="https://travis-ci.org/lord/wargo"><img src="https://travis-ci.org/lord/wargo.svg?branch=master" alt="Build Status"></a>
+</p>
--- src/cargo.js
+++ src/cargo.js
@@ -1,5 +1,6 @@
 const child_process = require('child_process')
 const log = require('./log')
+const fs = require('fs')
 
 const FIXLLVM = `
   brew install llvm;
@@ -12,16 +13,28 @@ const FIXLLVM = `
 module.exports = function(args) {
   let cmd = 'cargo ' + args.join(' ')
   log(`running '${cmd}'`)
-  try {
-    let res = child_process.execSync(cmd, {env: process.env})
-  } catch (e) {
-    if (e.stderr.toString().indexOf('dyld: Symbol not found: _futimens') >= 0) {
-      log('mac linker error detected, see https://github.com/kripken/emscripten/issues/5418')
-      log('performing fix...')
-      child_process.execSync(FIXLLVM, {stdio: [null, process.stdout, process.stderr], env: process.env})
-      log('rerunning cargo command...')
-      module.exports(args)
+  let res = child_process.exec(`${cmd} --color always`, {env: process.env, stdio: 'pipe'})
+  let outBuf = ""
+  res.stdout.on('data', (dat) => {
+    dat = dat.toString()
+    outBuf += dat
+    console.log(dat)
+  })
+  res.stderr.on('data', (dat) => {
+    dat = dat.toString()
+    outBuf += dat
+    console.warn(dat)
+  })
+  res.on('exit', (code, signal) => {
+    if (code !== 0) {
+      if (outBuf.indexOf('dyld: Symbol not found: _futimens') >= 0) {
+        log('mac linker error detected, see https://github.com/kripken/emscripten/issues/5418')
+        log('performing fix...')
+        child_process.execSync(FIXLLVM, {stdio: [null, process.stdout, process.stderr], env: process.env})
+        log('rerunning cargo command...')
+        module.exports(args)
+      }
+      process.exit(code)
     }
-    process.exit(e.status)
-  }
+  })
 }
