0aafe876bb867a2fa957c8c5c1bf5ff988f60f29
Robert Lord <robert@lord.io>
2017-10-22 14:42:42 -0500

    Warn if fail os check, don't just fail, fixes #6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 "use strict"
 const getos = require('getos')
+const log = require('./src/log')
 
 process.on('unhandledRejection', (error) => {
   console.warn('unhandledRejection:', error.message)
@@ -8,8 +9,12 @@ process.on('unhandledRejection', (error) => {
 })
 
 getos((e, os) => {
-  if(e) return console.error(e)
-  process.detailedos = os
+  if (e) {
+    log('warning: os identification error: ', e)
+    process.detailedos = {os: 'linux'}
+  } else {
+    process.detailedos = os
+  }
   let args = process.argv.slice(2)
   require('./src/cmd.js')(args)
 })
