86fdf2f1cf39bc21fe7ee5e4d2103989b7caa567
Robert Lord <robert@lord.io>
2017-10-27 13:57:39 -0500

    Print wargo prefix for commands instead of Setup wasm

--- src/log.js
+++ src/log.js
@@ -3,6 +3,6 @@
 const chalk = require('chalk')
 
 module.exports = function (...args) {
-  args.unshift(chalk.green.bold('  Setup wasm'))
+  args.unshift(chalk.green.bold('       wargo'))
   console.warn(...args)
 }
